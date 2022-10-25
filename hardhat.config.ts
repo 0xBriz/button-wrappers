e32f8d4fe6feab81843246e4e339ef28bb257863
0xBriz <chaindevone@gmail.com>
2022-10-25 13:04:05 -0400

    config update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,5 @@
 import './test/utils/loadDotEnv'
 import { HardhatUserConfig } from 'hardhat/config'
-import { Wallet } from 'ethers'
 import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-waffle'
 import '@openzeppelin/hardhat-upgrades'
@@ -9,44 +8,24 @@ import 'solidity-coverage'
 import 'hardhat-gas-reporter'
 import './tasks'
 
-const DEFAULT_MNEMONIC = Wallet.createRandom().mnemonic.phrase
-
 export default {
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
   },
   networks: {
-    rinkeby: {
-      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts: process.env.DEV_PKEY
-        ? [process.env.DEV_PKEY]
-        : {
-            mnemonic: process.env.DEV_MNEMONIC || DEFAULT_MNEMONIC,
-          },
-    },
-    kovan: {
-      url: `https://kovan.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts: process.env.DEV_PKEY
-        ? [process.env.DEV_PKEY]
-        : {
-            mnemonic: process.env.DEV_MNEMONIC || DEFAULT_MNEMONIC,
-          },
-    },
     goerli: {
-      url: `https://goerli.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts: process.env.DEV_PKEY
-        ? [process.env.DEV_PKEY]
-        : {
-            mnemonic: process.env.DEV_MNEMONIC || DEFAULT_MNEMONIC,
-          },
+      url: process.env.GOERLI_RPC,
+      accounts:
+        process.env.OPERATOR_KEY !== undefined
+          ? [process.env.OPERATOR_KEY]
+          : [],
     },
-    mainnet: {
-      url: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts: process.env.PROD_PKEY
-        ? [process.env.PROD_PKEY]
-        : {
-            mnemonic: process.env.PROD_MNEMONIC || DEFAULT_MNEMONIC,
-          },
+    bsc: {
+      url: process.env.BSC_RPC,
+      accounts:
+        process.env.OPERATOR_KEY !== undefined
+          ? [process.env.OPERATOR_KEY]
+          : [],
     },
   },
   solidity: {
